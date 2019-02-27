decfcd206ad3157bb047c1a31f4b6a8bf38a75d0
Nithin <nithin2889@gmail.com>
2019-02-27 20:16:37 +0530

    Reference Type Configuration updated.

--- springcore/src/main/java/com/nithin/spring/springcore/beans/Test.java
+++ springcore/src/main/java/com/nithin/spring/springcore/beans/Test.java
@@ -1,11 +1,12 @@
 package com.nithin.spring.springcore.beans;
 
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Test {
 	
 	public static void main(String[] args) {
-		ClassPathXmlApplicationContext ctx = new ClassPathXmlApplicationContext("config.xml");
+		ApplicationContext ctx = new ClassPathXmlApplicationContext("config.xml");
 		Employee emp = (Employee) ctx.getBean("emp");
 		System.out.println("Employee ID: " + emp.getId());
 		System.out.println("Employee Name: " + emp.getName());
